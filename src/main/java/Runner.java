1dd4344ab4e801a08da5301c053dc253e8fcf912
Sean Grimes <spg63@cs.drexel.edu>
2018-11-20 21:55:35 -0500

    Output runtimes at end

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,11 +1,14 @@
 import com.google.common.base.Stopwatch;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Runner {
     public static void main(String[] args) throws Exception {
         int numRuns = 5;
         long totalMS = 0L;
+        List<Long> runTimes = new ArrayList<>();
 
         for(int i = 0; i < numRuns; ++i){
             CSVExample csvExample = new CSVExample();
@@ -15,10 +18,13 @@ public class Runner {
             long runMS = timer.elapsed(TimeUnit.MILLISECONDS);
             System.out.println("Run #" + i + " took " + runMS + " ms");
             totalMS += runMS;
+            runTimes.add(runMS);
         }
 
         System.out.println("Total runtime for " + numRuns + " tests: " + totalMS + " ms.");
         System.out.println("Average runtime: " + (totalMS / numRuns) + " ms.");
+        for(int i = 0; i < numRuns; ++i)
+            System.out.println("Run " + i + " took " + runTimes.get(i) + " ms");
 
     }
 }
